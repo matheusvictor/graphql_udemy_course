2b58eb9f0fdc3297ce60dedc54d71e00f91ca142
Matheus Victor <matheusvictor.salles@gmail.com>
2024-02-01 08:49:24 -0300

    UUID como ID do usuário

--- mutation/projeto/data/db.js
+++ mutation/projeto/data/db.js
@@ -1,13 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+const random_uuid = uuidv4();
+
 const users = [
     {
-        id: 1,
+        id: random_uuid,
         name: "Matheus",
         email: "matheus@email.com",
         age: 25,
         profile_id: 1,
     },
     {
-        id: 2,
+        id: random_uuid,
         name: "Maria",
         email: "maria@email.com",
         age: 25,
