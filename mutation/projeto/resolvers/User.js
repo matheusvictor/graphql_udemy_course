8ca93a8a96f3d4bf588ee6ad9b4878c4caa7c2fb
Matheus Victor <matheusvictor.salles@gmail.com>
2024-02-09 15:13:15 -0300

    Remove atributos não utilizados

--- mutation/projeto/resolvers/User.js
+++ mutation/projeto/resolvers/User.js
@@ -1,9 +1,6 @@
 const { profileTypes } = require('../data/db');
 
 module.exports = {
-    salary(parentObj) {
-        return parentObj.income;
-    },
     profile(user) {
         const profileDescription = 
             profileTypes.filter(p => p.id === user.profile_id)
